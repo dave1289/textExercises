f766e13338567404298c4ca81d6c4984d7c04a2f
dave <davemcelhaney@gmail.com>
2022-08-15 21:52:27 -0400

    destructuring push

---
+++ destructuring.js
@@ -0,0 +1,26 @@
+8, 1846
+
+1846, 1659
+
+"Your name is alejandro and you like purple"
+"your name is melissa nd you like green"
+"your name is  and you like green"
+
+"maya, Marisa, Chi"
+
+"raindrops on roses"
+"whiskers on kittens"
+"bright copper kettles","warm woolen mittens","brown paper packages tied up with strings"
+
+
+[20, 10, 30]
+
+const {a, b} = obj;
+
+[arr[1],arr[2]] = [arr[2], arr[1]]
+
+const raceResults = ([first, second, third, ...rest]) {
+    return {
+        first, second, third, rest
+    }
+};
